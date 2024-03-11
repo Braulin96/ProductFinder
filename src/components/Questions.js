5d0ff088f71dc9035c07a687d3a8411ecf9cadac
Braulin Pires <braulinpires@hotmail.com>
2024-03-11 20:50:22 +0000

    clean and organize Questions section

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -3,19 +3,31 @@ import Onboarding from "./shared/Onboarding";
 
 const Questions = () => {
   const [name, setName] = useState("");
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
   return (
     <Onboarding>
-      <h2 className="pb-2" style={{ fontSize: "1.6rem" }}>
+      <label
+        htmlFor="nameInput"
+        className="pb-2"
+        style={{ fontSize: "1.6rem" }}
+      >
         What is your first name?
-      </h2>
+      </label>
       <input
-      onChange={(e)=>setName(e.target.value)}
+        id="nameInput"
         type="text"
+        value={name}
+        onChange={handleNameChange}
         className="px-4 py-2 rounded-lg border-tertiary-gray"
-        placeholder="type your name..."
+        placeholder="Type your name..."
       />
       <p style={{ fontSize: "1.1rem" }}>{name}</p>
     </Onboarding>
   );
 };
+
 export default Questions;
