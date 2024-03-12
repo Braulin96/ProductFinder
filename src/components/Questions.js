d778d098cc2fb538c89ba27d34e9ef029789a14e
Braulin Pires <braulinpires@hotmail.com>
2024-03-12 18:49:29 +0000

    added date of birthday input

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -3,11 +3,31 @@ import Onboarding from "./shared/Onboarding";
 import useQuestionsData from "./hooks/useQuestionsData";
 import FitPeople from "assets/images/fitPeople.jpeg";
 
+import dayjs from "dayjs";
+import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+
 const Questions = () => {
   const { showNext, setShowNext } = useQuestionsData();
   const [name, setName] = useState("");
   const handleNameChange = (e) => {
     setName(e.target.value);
+  };
+
+  //GoalsImput:
+  const [selectedOption, setSelectedOption] = useState(null);
+  const handleOptionChange = (value) => {
+    setSelectedOption(value);
+  };
+
+  //Personal Data
+  //sex:
+  const [selectedSex, setSelectedSex] = useState(""); // State to store the selected sex
+  // Function to handle changes in the selected sex
+  const handleSexChange = (e) => {
+    setSelectedSex(e.target.value); // Update the selected sex state
   };
 
   const NameImput = () => {
@@ -43,32 +63,66 @@ const Questions = () => {
         id: "exerciseIntensity_brokenSentences",
       },
     ];
+
     return (
       <div className="flex flex-col">
         <label className="text-xl"> Thanks {name}! Now your goals.</label>
-        <p className="text-sm mt-2">Select one option that is better in your case.</p>
-        {answers.map((question) => (
-          <a
+        <p className="text-sm mt-2">
+          Select one option that is better in your case.
+        </p>
+        {answers.map((answer) => (
+          <label
             className="mt-4 border border-black p-4 w-full"
-            key={question.value}
+            key={answer.value}
           >
-            {question.text}
-          </a>
+            <input
+              type="radio"
+              name="goal"
+              value={answer.text}
+              checked={selectedOption === answer.text}
+              onChange={() => handleOptionChange(answer.text)}
+            />
+            {answer.text}
+          </label>
         ))}
       </div>
     );
   };
 
-  const RaisonImput = () => {
+  const PersonalData = () => {
     return (
-      <div className="">
-        <label className="text-xl"> What is the raison for losing weight</label>
-        <p>Select up to 3</p>
-        <input
-          placeholder="type your name..."
-          type="text"
-          className="mt-6 w-full rounded-lg border border-gray-400 px-4 py-2"
-        />
+      <div className="flex flex-col">
+        <label className="text-xl">Select sex:</label>
+        <div className="flex gap-x-8">
+          <label className="inline-flex items-center mt-2">
+            <input
+              type="radio"
+              value="male"
+              checked={selectedSex === "male"} // Set checked based on selected sex
+              onChange={handleSexChange} // Handle change event
+              className="form-radio h-5 w-5 text-blue-600"
+            />
+            <span className="ml-2">Male</span>
+          </label>
+          <label className="inline-flex items-center mt-2">
+            <input
+              type="radio"
+              value="female"
+              checked={selectedSex === "female"} // Set checked based on selected sex
+              onChange={handleSexChange} // Handle change event
+              className="form-radio h-5 w-5 text-pink-600"
+            />
+            <span className="ml-2">Female</span>
+          </label>
+
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DemoContainer components={["DatePicker"]}>
+              <DemoItem label="Desktop variant">
+                <DatePicker  />
+              </DemoItem>
+            </DemoContainer>
+          </LocalizationProvider>
+        </div>
       </div>
     );
   };
@@ -87,7 +141,7 @@ const Questions = () => {
     {
       image: FitPeople,
       title: "example 3",
-      description: RaisonImput(),
+      description: PersonalData(),
     },
   ];
 
