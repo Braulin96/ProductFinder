60949bd58d12a0418518a6e05ca6701aea62985c
Braulin Pires <braulinpires@hotmail.com>
2024-03-12 16:28:37 +0000

    pass all the others components into the main component, in this case Questions component

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -3,93 +3,75 @@ import Onboarding from "./shared/Onboarding";
 import useQuestionsData from "./hooks/useQuestionsData";
 import FitPeople from "assets/images/fitPeople.jpeg";
 
-const NameImput = () => {
-  return (
-    <div className="">
-      <label className="text-xl"> What is you first name</label>
-      <input
-        placeholder="type your name..."
-        type="text"
-        className="mt-6 w-full rounded-lg border border-gray-400 px-4 py-2"
-      />
-    </div>
-  );
-};
-
-const GoalsImput = () => {
-  return (
-    <div className="">
-      <label className="text-xl"> Thanks Jorge, Now your goals.</label>
-      <p>Select up to 3</p>
-      <input
-        placeholder="type your name..."
-        type="text"
-        className="mt-6 w-full rounded-lg border border-gray-400 px-4 py-2"
-      />
-    </div>
-  );
-};
-
-const RaisonImput = () => {
-  return (
-    <div className="">
-      <label className="text-xl"> What is the raison for losing weight</label>
-      <p>Select up to 3</p>
-      <input
-        placeholder="type your name..."
-        type="text"
-        className="mt-6 w-full rounded-lg border border-gray-400 px-4 py-2"
-      />
-    </div>
-  );
-};
-
-const elements = [
-  {
-    image: FitPeople,
-    title: "",
-    description: NameImput(),
-  },
-  {
-    image: FitPeople,
-    title: "example 2",
-    description: GoalsImput(),
-  },
-  {
-    image: FitPeople,
-    title: "example 3",
-    description: RaisonImput(),
-  },
-];
-
 const Questions = () => {
   const { showNext, setShowNext } = useQuestionsData();
   const [name, setName] = useState("");
-
   const handleNameChange = (e) => {
     setName(e.target.value);
   };
 
-  return (
-    <Onboarding elements={elements}>
-      <label
-        htmlFor="nameInput"
-        className="pb-2"
-        style={{ fontSize: "1.6rem" }}
-      >
-        What is your first name?
-      </label>
-      <input
-        id="nameInput"
-        type="text"
-        value={name}
-        onChange={handleNameChange}
-        className="px-4 py-2 rounded-lg border-tertiary-gray"
-        placeholder="Type your name..."
-      />
-      <p style={{ fontSize: "1.1rem" }}>{name}</p>
-    </Onboarding>
-  );
+  const NameImput = () => {
+    return (
+      <div className="">
+        <label className="text-xl"> What is you first name</label>
+        <input
+          placeholder="type your name..."
+          type="text"
+          value={name}
+          onChange={handleNameChange}
+          className="mt-6 w-full rounded-lg border border-gray-400 px-4 py-2"
+        />
+      </div>
+    );
+  };
+
+  const GoalsImput = () => {
+    return (
+      <div className="">
+        <label className="text-xl"> Thanks {name} , Now your goals.</label>
+        <p>Select up to 3</p>
+        <input
+          placeholder="type your name..."
+          type="text"
+          className="mt-6 w-full rounded-lg border border-gray-400 px-4 py-2"
+        />
+      </div>
+    );
+  };
+
+  const RaisonImput = () => {
+    return (
+      <div className="">
+        <label className="text-xl"> What is the raison for losing weight</label>
+        <p>Select up to 3</p>
+        <input
+          placeholder="type your name..."
+          type="text"
+          className="mt-6 w-full rounded-lg border border-gray-400 px-4 py-2"
+        />
+      </div>
+    );
+  };
+
+  const elements = [
+    {
+      image: FitPeople,
+      title: "",
+      description: NameImput(),
+    },
+    {
+      image: FitPeople,
+      title: "example 2",
+      description: GoalsImput(),
+    },
+    {
+      image: FitPeople,
+      title: "example 3",
+      description: RaisonImput(),
+    },
+  ];
+
+  return <Onboarding elements={elements} />;
 };
 
 export default Questions;
