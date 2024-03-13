d3e77c9e9bd5a8270dffa95739f1332297eeeea4
Braulin Pires <braulinpires@hotmail.com>
2024-03-13 10:46:32 +0000

    removed import not used

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import Questions from "components/Questions";
-import Onboarding from "components/shared/Onboarding";
 import Welcome from "components/Welcome";
 
 function App() {
   return (
     <div className="App text-red-500">
-      {/* <Welcome /> */}
+      <Welcome />
       {/* <Onboarding/> */}
       <Questions/>
     </div>
