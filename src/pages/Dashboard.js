f9bffd3aef8816aff8cc0fd52ae1a21c01b15315
Braulin Pires <braulinpires@hotmail.com>
2024-03-14 10:11:41 +0000

    added option for button to clean the local storage

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,9 +3,26 @@ import { useData } from "components/hooks/DataContext";
 
 const Dashboard = () => {
   const { name, goal, sex, age } = useData();
+  const cleanLocalStorage = () => {
+    localStorage.clear();
+  };
   return (
     <div>
-      My name is {name} I am {sex}, I am {age} years old and I want {goal}
+      <p>
+        My name is {name} I am {sex}, I am {age} years old and I want {goal}
+      </p>
+      <div>
+        Is that right?
+        <div className="flex">
+          <button
+            onClick={cleanLocalStorage}
+            className="p-4 rounded-lg border-2 border-black"
+          >
+            Clear Data
+          </button>
+          <button className="p-4 rounded-lg border-2 border-black">Edit</button>
+        </div>
+      </div>
     </div>
   );
 };
