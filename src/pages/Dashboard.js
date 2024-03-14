e78c537d215082fd53d356d4d938e593627d813b
Braulin Pires <braulinpires@hotmail.com>
2024-03-14 16:57:01 +0000

    installed axios and getting images using api

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,43 @@
 import { useData } from "components/hooks/DataContext";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Dashboard = () => {
-    const { name, goal, sex, age } = useData();
+  //const [items, setItems] = useState([]);
+
+  const [randomImage, setRandomImage] = useState("");
+  const [time, setTime] = useState("");
+
+  const fetchRandomImage = async () => {
+    try {
+      const response = await fetch("https://source.unsplash.com/random");
+      // Get the final URL after any redirects (actual image URL)
+      const imageUrl = response.url;
+      setRandomImage(imageUrl);
+    } catch (error) {
+      console.error("Error fetching random image:", error);
+    }
+  };
+  useEffect(() => {
+    fetchRandomImage();
+  }, [time]);
+
+  const { name, goal, sex, age } = useData();
   return (
-    <div>Hello {name} welcome to your dashboard app where you can, you are a {sex} with {age} years olt trying to {goal}
+    <div>
+      Hello {name} welcome to your dashboard app where you can, you are a {sex}{" "}
+      with {age} years olt trying to {goal}
+      <div className="w-96 h-96 rounded-full bg-cover overflow-hidden mt-12">
+        {randomImage && (
+          <img
+            className="w-full h-full object-cover"
+            src={randomImage}
+            alt="Random"
+          />
+        )}
+        {!randomImage && <p className="text-black">Loading random image...</p>}
+      </div>
     </div>
-  )
-}
-export default Dashboard
+  );
+};
+export default Dashboard;
