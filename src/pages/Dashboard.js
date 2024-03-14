b83d0de2fb746a635425fc349aa22d97ad8fdd96
Braulin Pires <braulinpires@hotmail.com>
2024-03-14 11:20:16 +0000

    remove elements not used in Dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,10 @@
 import { useData } from "components/hooks/DataContext";
 
 const Dashboard = () => {
-    const { name, goal, sex, age, setName, setGoal, setSex } = useData();
+    const { name, goal, sex, age } = useData();
   return (
-    <div>Hello {name} welcome to your dashboard app where you can...</div>
+    <div>Hello {name} welcome to your dashboard app where you can, you are a {sex} with {age} years olt trying to {goal}
+    </div>
   )
 }
 export default Dashboard
