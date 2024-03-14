00023acfe4b9a7eada4bd18f8a08cf6ec1b34611
Braulin Pires <braulinpires@hotmail.com>
2024-03-14 09:58:38 +0000

    elements passed into local storage but age is failling

--- src/components/hooks/useQuestionsData.js
+++ src/components/hooks/useQuestionsData.js
@@ -1,27 +1,60 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const useQuestionsData = () => {
-  //State variables
-  const [name, setName] = useState("");
-  const [goal, setGoal] = useState(null);
-  const [sex, setSex] = useState("");
+  // Retrieve data from local storage on initial load
+  const initialName = localStorage.getItem("name") || "";
+  const initialGoal = localStorage.getItem("goal") || null;
+  const initialSex = localStorage.getItem("sex") || "";
+  // const initialSelectedDate = localStorage.getItem("selectedDate")
+  //   ? new Date(localStorage.getItem("selectedDate"))
+  //   : null;
+
+  // State variables
+  const [name, setName] = useState(initialName);
+  const [goal, setGoal] = useState(initialGoal);
+  const [sex, setSex] = useState(initialSex);
+  const [selectedDate, setSelectedDate] = useState(null);
 
   // Event handlers
-  const handleNameChange = (e) => setName(e.target.value);
-  const handleOptionChange = (value) => setGoal(value);
-  const handleSexChange = (e) => setSex(e.target.value);
+  const handleNameChange = (e) => {
+    const newName = e.target.value;
+    setName(newName);
+    localStorage.setItem("name", newName);
+  };
+  
+  const handleOptionChange = (value) => {
+    setGoal(value);
+    localStorage.setItem("goal", value);
+  };
+  
+  const handleSexChange = (e) => {
+    const newSex = e.target.value;
+    setSex(newSex);
+    localStorage.setItem("sex", newSex);
+  };
 
-  //Calculate DOB
-  const currentDate = new Date();
-  const currentYear = currentDate.getFullYear();
-  const [selectedDate, setSelectedDate] = useState(null);
   const handleDateChange = (date) => {
     setSelectedDate(date);
+    localStorage.setItem("selectedDate", date);
   };
+
+  // Calculate age based on selected date
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
   const birthdayYear = selectedDate
     ? new Date(selectedDate).getFullYear()
     : null;
   const age = birthdayYear ? currentYear - birthdayYear : null;
+
+  useEffect(() => {
+    // Update local storage whenever state changes
+    localStorage.setItem("name", name);
+    localStorage.setItem("goal", goal);
+    localStorage.setItem("sex", sex);
+    if (selectedDate) {
+      localStorage.setItem("selectedDate", selectedDate.toISOString());
+    }
+  }, [name, goal, sex, age]);
 
   return {
     name,
@@ -35,4 +68,5 @@ const useQuestionsData = () => {
     handleDateChange,
   };
 };
+
 export default useQuestionsData;
