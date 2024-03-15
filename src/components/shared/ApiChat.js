1fc4be9c9f31705ff8c0e8f03e86a4dd96f5b812
Braulin Pires <braulinpires@hotmail.com>
2024-03-15 11:42:15 +0000

    added the layout for the chat by using chatscope

--- src/components/shared/ApiChat.js
+++ src/components/shared/ApiChat.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import { useState } from "react";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from "@chatscope/chat-ui-kit-react";
 
 const ApiChat = () => {
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I am",
+      sender: "ChatGPT",
+    },
+  ]);
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message: message,
+      sender: "user",
+      direction: "outgoing", // it is a message we are sending out
+    };
+    const newMessages = [...messages, newMessage]; //all the old messages + the new messages
+    setMessages(newMessages);
+  };
+
   return (
-    <div>ApiChat</div>
-  )
-}
-export default ApiChat
+    <div style={{ position: "relative", height: "500px" }}>
+      <MainContainer>
+        <ChatContainer>
+          <MessageList>
+            {messages.map((message, i) => {
+              return <Message key={i} model={message} />;
+            })}
+          </MessageList>
+          <MessageInput placeholder="Type message here" onSend={handleSend} />
+        </ChatContainer>
+      </MainContainer>
+    </div>
+  );
+};
+export default ApiChat;
