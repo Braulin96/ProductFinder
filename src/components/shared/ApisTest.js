81176ceea8839c2d273be2fb07199c099a90f0e7
Braulin Pires <braulinpires@hotmail.com>
2024-03-15 07:53:07 +0000

    basice fetch api

--- src/components/shared/ApisTest.js
+++ src/components/shared/ApisTest.js
@@ -1,9 +1,21 @@
 import React from "react";
 
 function ApisTest() {
-  fetch("https://reqres.in/api/users")
-    .then((res) => res.json())
-    .then((data) => console.log(data)); // converter dados para json type
+  fetch("https://reqres.in/api/users/1000")
+    .then((res) => {
+      if (res.ok) {
+        return res.json(); // convert to JSON and return
+      } else {
+        console.log("Unsuccess");
+        //throw new Error("Unsuccessful request"); 
+      }
+    })
+    .then((data) => {
+      console.log(data); // log the JSON data
+      // console.log(data.data[0].first_name); // to get the data inside the 'data' property at position 0 and then 'first_name'
+    })
+    .catch((err) => console.log(err)); // will only execute if there's an error during the network request itself 
+
   return <div>ApisTest</div>;
 }
 
