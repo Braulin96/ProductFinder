e9aac5187be543e4c66630e2da938cf500bf09e2
Braulin Pires <braulinpires@hotmail.com>
2024-03-16 17:50:37 +0000

    add new images to  use in landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -6,6 +6,7 @@ import "swiper/css";
 //images:
 import FitPeople from "assets/images/fitPeople.jpeg";
 import FitWoman from "assets/images/fitWoman.webp";
+import Woman from "assets/images/secondWomen.jpeg";
 
 const elements = [
   {
@@ -28,11 +29,14 @@ const elements = [
 
 const Landing = () => {
   return (
-    <div className="flex items-center" style={{ height: "100vh" }}>
-      <div className="bg-green-200 w-full max-w-7xl mx-auto flex md:flex-row flex-col h-96 gap-y-4">
-        <div className="md:w-1/2 w-full bg-blue-400 h-full text-center flex flex-col gap-y-8">
-          <h1 className="text-black"> Welcome to Product Finder</h1>
-          <p>
+    <div
+      className="flex flex-col items-center bg-gray-100 relative"
+      style={{ height: "100vh" }}
+    >
+      <div className="my-auto md:px-12 py-12 md:rounded-full w-full max-w-7xl mx-auto flex md:flex-row flex-col gap-y-4 z-20 bg-gray-50 bg-opacity-60 shadow-xl">
+        <div className="md:w-1/2 w-full h-full text-center flex flex-col gap-y-8 text-gray-800">
+          <h1 className="text-4xl"> Welcome to Product Finder</h1>
+          <p className="text-lg text-left">
             Product Finder is an online platform where you can discover a wide
             range of products including phones, laptops, tablets, and
             smartwatches. Explore our collection and find the perfect product to
@@ -42,7 +46,7 @@ const Landing = () => {
             Let's start
           </button>
         </div>
-        <div className="md:w-1/2 w-full bg-red-300 h-full">
+        <div className="md:w-1/2 w-full h-full">
           <Swiper
             modules={[Autoplay]}
             spaceBetween={50}
@@ -57,6 +61,7 @@ const Landing = () => {
               <SwiperSlide key={images.name}>
                 <div className="w-auto h-96 aspect-square mx-auto rounded-full flex">
                   <img
+                    style={{ opacity: "0.8" }}
                     className="rounded-full mx-auto"
                     src={images.image}
                     alt="FitPeople"
@@ -66,6 +71,9 @@ const Landing = () => {
             ))}
           </Swiper>
         </div>
+      </div>
+      <div className="absolute z-10 h-full">
+        <img style={{ height: "100vh", opacity: "0.6" }} src={Woman} />
       </div>
     </div>
   );
