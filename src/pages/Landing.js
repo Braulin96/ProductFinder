313a4c4bfbdfdd8e5984ce132d2023e62c775ea9
Braulin Pires <braulinpires@hotmail.com>
2024-03-16 16:21:51 +0000

    added new estructure for the landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -4,68 +4,16 @@ import { Link } from "react-router-dom";
 import FitPeople from "assets/images/fitPeople.jpeg";
 import FitWoman from "assets/images/fitWoman.webp";
 
-const moduleData = [
-  {
-    image: FitPeople,
-    description: "Ready for some Wins? Start tracking, it is easy!",
-  },
-  {
-    image: FitWoman,
-    description: "second message!",
-  },
-  {
-    image: FitPeople,
-    description: "Third message",
-  },
-];
-
-const Module = ({ backgroundImage, description }) => {
-  return (
-    <div className="sm:w-64 min-w-64 w-full h-72 rounded-lg flex flex-col relative overflow-hidden border-gray-100 shadow-md">
-      <div
-        className="my-auto h-full flex"
-        style={{
-          backgroundImage: `url(${backgroundImage})`,
-          backgroundSize: "cover",
-        }}
-      ></div>
-
-      <div className="bg-white w-full px-8 h-16  my-auto flex text-center justify-center">
-        <p className="my-auto text-secondary-gray">{description}</p>
-      </div>
-    </div>
-  );
-};
-
 const Landing = () => {
   return (
-    <div className="my-auto h-full bg-tertiary-gray lg:py-32 md:py-24 sm:py-12 py-8 flex text-secondary-gray">
-      <div className="max-w-7xl w-full h-full mx-auto flex my-auto">
-        <div className="m-auto text-center w-full">
-          <p className="text-2xl">
-            Welcome to <br />
-            <span className="text-4xl font-semibold">
-              My Fitness <span className="text-primary-blue">Grow</span>
-            </span>
-          </p>
-          <div className="mt-12 flex justify-center flex-wrap w-full md:px-24 gap-x-12 px-8 gap-y-8">
-            {moduleData.map((data, index) => (
-              <Module
-                key={index}
-                backgroundImage={data.image}
-                description={data.description}
-              />
-            ))}
-          </div>
-          <div className="px-8 mt-12">
-            <Link
-              to="questions"
-              className="bg-primary-blue rounded-lg sm:w-64 min-w-64 w-full py-1.5 text-white font-semibold text-lg"
-            >
-              Continue
-            </Link>
-          </div>
+    <div className="flex items-center" style={{ height: "100vh" }}>
+      <div className="bg-green-200 w-full max-w-7xl mx-auto flex md:flex-row flex-col h-96 gap-y-4">
+        <div className="md:w-1/2 w-full bg-blue-400 h-full text-center flex flex-col gap-y-8">
+          <h1 className="text-black"> Welcome to Product Finder</h1>
+          <p> Product Finder is an online platform where you can discover a wide range of products including phones, laptops, tablets, and smartwatches. Explore our collection and find the perfect product to suit your needs.</p>
+          <button className="px-8 sm:w-fit w-full mx-auto py-2 border-2 border-gray-200 rounded-full"> Let's start</button>
         </div>
+        <div className="md:w-1/2 w-full bg-red-300 h-full">container 2</div>
       </div>
     </div>
   );
