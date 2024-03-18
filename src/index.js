be370d62a5f1b431667bb377314facd433a099f1
Braulin Pires <braulinpires@hotmail.com>
2024-03-18 19:25:09 +0000

    deleted API Chat

--- src/index.js
+++ src/index.js
@@ -3,17 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import Landing from "pages/Landing";
-import Contact from "components/Contact";
 import UserData from "pages/UserData";
 import ErrorPage from "pages/ErrorPage";
-
 import Dashboard from "pages/Dashboard";
-import ApisTest from "./components/shared/ApisTest.js";
-import ApiReact from "components/shared/ApiReact";
-import ApiChat from "components/shared/ApiChat";
-import RevenueAPI from "components/shared/RevenueAPI";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
@@ -30,18 +23,10 @@ const router = createBrowserRouter([
         path: "/questions",
         element: <UserData />,
       },
-    
+
       {
         path: "/dashboard",
         element: <Dashboard />,
-      },
-      {
-        path: "/api",
-        element: <ApiChat />,
-      },
-      {
-        path: "/revenue",
-        element: <RevenueAPI />,
       },
     ],
   },
