1e53c03d8bd379c45fe44af77f740ec8c59ff849
Braulin Pires <braulinpires@hotmail.com>
2024-03-18 18:16:05 +0000

    removed all imports and data not used

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,14 +1,14 @@
+//components:
 import { useData } from "components/hooks/DataContext";
-import axios from "axios";
-import { useState, useEffect } from "react";
 import useRevenueData from "../components/shared/useRevenueData";
 import OpenModal from "components/shared/OpenModal";
 import Carousel from "components/shared/Carousel";
 
 const ProductsByCategory = () => {
   const { data, isLoading, isError } = useRevenueData();
-  console.log("dataCategory:", data);
   // isLoading and isError you do not need to declare, automatically from react-query
+  console.log("dataCategory:", data);
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -16,10 +16,7 @@ const ProductsByCategory = () => {
     return <div>Error fetching data</div>;
   }
   return (
-    <div
-      className="w-full"
-      //style={{ height: "100vh" }}
-    >
+    <div className="w-full">
       <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-y-8 gap-x-4 py-4">
         {data.map((recipe) => (
           <div
@@ -27,18 +24,12 @@ const ProductsByCategory = () => {
             key={recipe.title}
           >
             <div className="overflow-hidden w-1/2 my-auto px-2 relative">
-              {/* {recipe.images.length > 0 && ( // Check if images array is not empty
-                  <img className="" src={recipe.images[0]} alt={`Image`} /> // Render only the first image
-                )}   */}
               <img className="" src={recipe.thumbnail} alt={`Image`} />
             </div>
             <div className="px-6 flex flex-col w-1/2 my-auto h-full">
               <div className="my-auto">
-                {/* <p>Id:{recipe.id}</p> */}
-                {/* <p>Brand:{recipe.brand}</p> */}
                 <p className="mt-0 capitalize"> {recipe.title}</p>
                 <p>Price: {recipe.price} £</p>
-                {/* <p>Category: {recipe.category}</p> */}
               </div>
               <div className="">
                 <OpenModal
@@ -65,11 +56,6 @@ const ProductsByCategory = () => {
                 </OpenModal>
               </div>
             </div>
-            {/* <div>
-                {recipe.images.map((image, index) => (
-                  <img key={index} src={image} alt={`Image ${index}`} />
-                ))}
-              </div> */}
           </div>
         ))}
       </div>
@@ -78,13 +64,7 @@ const ProductsByCategory = () => {
 };
 
 const Dashboard = () => {
-  const { name, goal, sex, age } = useData();
-  //const [items, setItems] = useState([]);
-  //const response = await fetch("https://source.unsplash.com/random");
-
-  // fetch("https://reqres.in/api/users")
-  //   .then((res) => res.json())
-  //   .then((data) => console.log(data)); // converter dados para json type
+  const { name } = useData();
 
   return (
     <div
