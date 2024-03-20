397feda9a53cd8921faaeca87e34d3d7b37895f8
Braulin Pires <braulinpires@hotmail.com>
2024-03-20 12:29:24 +0000

    added animated tailwind css plugin

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -46,15 +46,15 @@ const Home = () => {
       className="flex flex-col items-center relative sm:px-4 bg-[#D9D9D9] px-2 sm:py-0 py-2"
       style={{ height: "100vh" }}
     >
-      <div className="my-auto md:px-12 py-12 rounded-3xl w-full max-w-7xl mx-auto flex md:flex-row flex-col gap-y-8 z-20 bg-gray-50 bg-opacity-60 shadow-xl gap-x-4 px-4">
+      <div className="my-auto md:px-12 py-12 rounded-3xl w-full max-w-7xl mx-auto flex md:flex-row flex-col gap-y-8 z-20 bg-gray-50 bg-opacity-60 shadow-xl gap-x-4 px-4 animate-fade-up animate-duration-500 animate-delay-500">
         <div className="md:w-1/2 w-full h-full text-center flex flex-col sm:gap-y-8 gap-y-4 text-secondary-gray my-auto justify-center">
-          <h1 className="sm:text-4xl text-3xl">
+          <h1 className="sm:text-4xl text-3xl animate-fade-up animate-duration-[1s] animate-delay-1000">
             Welcome to
             <span className="text-primary-blue font-bold ml-2">
               Product Finder
             </span>
           </h1>
-          <p className="sm:text-lg text-left">
+          <p className="sm:text-lg text-left animate-fade-up animate-duration-[1s] animate-delay-[1.2s]">
             Product Finder is an online platform where you can discover a wide
             range of products including phones, laptops, tablets, and
             smartwatches. Explore our collection and find the perfect product to
@@ -62,12 +62,12 @@ const Home = () => {
           </p>
           <Link
             to="questions"
-            className="hidden slide md:flex px-16 sm:w-fit w-full mx-auto py-2 rounded-lg bg-primary-blue text-white font-bold mt-4"
+            className="hidden slide md:flex px-16 sm:w-fit w-full mx-auto py-2 rounded-lg bg-primary-blue text-white font-bold mt-4 animate-fade-up animate-duration-[1s] animate-delay-[1.6s]"
           >
             Let's Start
           </Link>
         </div>
-        <div className="md:w-1/2 w-full h-full flex">
+        <div className="md:w-1/2 w-full h-full flex animate-fade-left animate-duration-[1s] animate-delay-[2.2s]">
           <Swiper
             className="my-auto"
             modules={[Autoplay]}
@@ -100,8 +100,8 @@ const Home = () => {
       <div className="absolute z-10 h-full md:flex hidden">
         <div className="relative">
           <img style={{ height: "100vh", opacity: "0.7" }} src={Woman} />
-          <div className="absolute w-12 aspect-square bg-primary-blue rounded-full z-40 top-20 right-8 bg-opacity-40"></div>
-          <div className="absolute w-24 aspect-square bg-[#d5ae83] rounded-full z-40 bottom-8 left-8 bg-opacity-40"></div>
+          <div className="absolute w-12 aspect-square bg-primary-blue rounded-full z-40 top-20 right-8 bg-opacity-40 animate-fade-down animate-duration-[1s] animate-delay-[2.6s]"></div>
+          <div className="absolute w-24 aspect-square bg-[#d5ae83] rounded-full z-40 bottom-8 left-8 bg-opacity-40 animate-fade-up animate-duration-[1s] animate-delay-[3s]"></div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        'primary-blue': "#1976d2",
+        "primary-blue": "#1976d2",
         //'primary-sage': "#1976d2",
-        'secondary-sage': "#c8d0ba",
-        'secondary-gray': '#424242',
-        
-        'tertiary-gray': "#F0F0F0",
-      }
+        "secondary-sage": "#c8d0ba",
+        "secondary-gray": "#424242",
+
+        "tertiary-gray": "#F0F0F0",
+      },
     },
   },
   plugins: [
-		require("tailwindcss-animate"),
-		// ...
-	],
-}
-
+    require("tailwindcss-animated"),
+  ],
+};
