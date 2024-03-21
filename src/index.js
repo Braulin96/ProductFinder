de57df66e0f88ad6e5410bf020bacb819b77bc58
Braulin Pires <braulinpires@hotmail.com>
2024-03-21 10:04:50 +0000

    added absolute path in some imports in App, Index and Questions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import "index.css";
+import App from "App";
 import Home from "pages/Home";
 import Questions from "pages/Questions";
 import NotFound from "pages/NotFound";
